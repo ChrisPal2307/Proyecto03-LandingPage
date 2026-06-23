83a5faae9859fad35a554c3f49ac9b741b9dd042
JulioLlerena1 <juceller@espol.edu.ec>
2026-06-22 21:19:03 -0500

    Add appointment counter and minor formatting fixes
    Add a count() function in src/js/index.js that fetches the realtime DB serviceRequests.json and updates the #total-citas-contador element with the number of stored requests. Call count() on init (IIFE) so the counter is populated on load. Also include small HTML/JS formatting fixes: adjust span tag whitespace in index.html and minor import/indentation cleanups in index.js.

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-import {saveForm} from "./firebase.js";
+import { saveForm } from "./firebase.js";
 
 const enableform = () => {
-  const form = document.getElementById("saveForm");
+    const form = document.getElementById("saveForm");
 
     form.addEventListener("submit", async (event) => {
 
@@ -14,14 +14,14 @@ const enableform = () => {
             service: form.querySelector("#form-service").value,
             date: form.querySelector("#form-date").value,
             details: form.querySelector("#form-details").value
-        };  
+        };
 
         try {
 
             let response = await saveForm(formData);
 
             if (response.success) {
-                
+
                 alert(response.message);
                 form.reset();
 
@@ -40,9 +40,22 @@ const enableform = () => {
     );
 };
 
+const count = async () => {
+
+    const countForm = await fetch("https://landingpage-7afca-default-rtdb.firebaseio.com/serviceRequests.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const countElement = document.getElementById("total-citas-contador");
+            if (countElement) {
+                countElement.textContent = Object.keys(data).length;
+            }
+        });
+
+}
 
 (() => {
-  enableform();
+    enableform();
+    count();
 })();
 
 export { enableform };
